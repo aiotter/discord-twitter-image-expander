e1b2c06d1cb28a20d8ecfecf3f270d447be7f70e
aiotter <git@aiotter.com>
2021-07-03 03:48:25 +0900

    Add "Fetch original size" button

--- mod.ts
+++ mod.ts
@@ -13,33 +13,61 @@ import {
   startBot,
 } from "https://deno.land/x/discordeno@11.2.0/mod.ts";
 
+enum twitterImageSize {
+  Thumbnail = "thumb",
+  Small = "small",
+  Medium = "medium",
+  Large = "large",
+  Original = "orig",
+}
+
 const twitterUrlRegex = new RegExp(
   "https?://twitter.com/([^/\\s]+)/status/([0-9]+)",
 );
 
-class CustomId {
+class ButtonCustomId {
   twitterUserName: string;
   twitterStatusId: string;
   discordMessageId: string;
+  imageSize: twitterImageSize;
   constructor(
     twitterUserName: string,
     twitterStatusId: string,
     discordMessageId: string,
+    imageSize: twitterImageSize,
   ) {
     this.twitterUserName = twitterUserName;
     this.twitterStatusId = twitterStatusId;
     this.discordMessageId = discordMessageId;
+    this.imageSize = imageSize;
   }
   toString() {
-    return `${this.twitterUserName}/${this.twitterStatusId}/${this.discordMessageId}`;
+    return [
+      this.twitterUserName,
+      this.twitterStatusId,
+      this.discordMessageId,
+      this.imageSize,
+    ].join("/");
+  }
+  toStatusUrl() {
+    return `https://twitter.com/${this.twitterUserName}/status/${this.twitterStatusId}`;
   }
   static fromString(str: string) {
-    const [twitterUserName, twitterStatusId, discordMessageId] = str.split("/");
-    return new this(twitterUserName, twitterStatusId, discordMessageId);
+    const [twitterUserName, twitterStatusId, discordMessageId, imageSize] = str
+      .split("/");
+    return new this(
+      twitterUserName,
+      twitterStatusId,
+      discordMessageId,
+      imageSize as twitterImageSize,
+    );
   }
 }
 
-function getImageUrlMap(message: DiscordenoMessage) {
+function getImageUrlMap(
+  message: DiscordenoMessage,
+  size: twitterImageSize = twitterImageSize.Large,
+) {
   const twitterUrls = new Set<string>();
   message.embeds.forEach((embed) => {
     const twitterUrl = embed?.url?.match(twitterUrlRegex);
@@ -51,7 +79,8 @@ function getImageUrlMap(message: DiscordenoMessage) {
     const [_statusUrl, userId, statusId] = match;
     const imageUrlsForStatusId = message.embeds
       .filter((e) => e?.url?.startsWith(twitterUrl) && e?.image)
-      .map((e) => e.image?.url) as string[];
+      .map((e) => e.image?.url)
+      .map((url) => url?.replace(/:([^/]+)?$/, `:${size}`)) as string[];
     imageUrls[`${userId}/${statusId}`] = imageUrlsForStatusId;
   }
   return imageUrls;
@@ -61,46 +90,67 @@ async function replyToTwitterWithMultipleImages(message: DiscordenoMessage) {
   const imageUrlMap = getImageUrlMap(message);
   for (const [tweetId, imageUrls] of Object.entries(imageUrlMap)) {
     const [twitterUserName, twitterStatusId] = tweetId.split("/");
-    const customId = new CustomId(twitterUserName, twitterStatusId, message.id.toString());
-    if (imageUrls.length > 1) {
-      const showImagesButton: ButtonComponent = {
-        type: DiscordMessageComponentTypes.Button,
-        label: `Show all the images (${imageUrls.length})`,
-        style: DiscordButtonStyles.Primary,
-        customId: customId.toString(),
+    const showImagesButtonCustomId = new ButtonCustomId(
+      twitterUserName,
+      twitterStatusId,
+      message.id.toString(),
+      twitterImageSize.Large,
+    );
+    const showImagesButton: ButtonComponent = {
+      type: DiscordMessageComponentTypes.Button,
+      label: `Show all the images (${imageUrls.length})`,
+      style: DiscordButtonStyles.Primary,
+      customId: showImagesButtonCustomId.toString(),
+    };
+
+    const fetchOriginalImagesButtonCustomId = new ButtonCustomId(
+      twitterUserName,
+      twitterStatusId,
+      message.id.toString(),
+      twitterImageSize.Original,
+    );
+    const fetchOriginalImagesButton: ButtonComponent = {
+      type: DiscordMessageComponentTypes.Button,
+      label: "Fetch original size",
+      style: DiscordButtonStyles.Secondary,
+      customId: fetchOriginalImagesButtonCustomId.toString(),
+    };
+
+    const openTwitterButton: ButtonComponent = {
+      type: DiscordMessageComponentTypes.Button,
+      label: "Open App",
+      style: DiscordButtonStyles.Link,
+      url: showImagesButtonCustomId.toStatusUrl(),
+    };
+
+    const row: ActionRow = {
+      type: DiscordMessageComponentTypes.ActionRow,
+      components: [
+        showImagesButton,
+        fetchOriginalImagesButton,
+        openTwitterButton,
+      ],
+    };
+    let sendingMessage: CreateMessage;
+    if (Object.keys(imageUrlMap).length > 1) {
+      sendingMessage = {
+        // Send thumbnail image if more than one tweet url is included
+        // in the original message
+        content: imageUrls[0].replace(/(:[^/]*?)?$/, ":thumb"),
+        components: [row],
       };
-      const openTwitterButton: ButtonComponent = {
-        type: DiscordMessageComponentTypes.Button,
-        label: "Open App",
-        style: DiscordButtonStyles.Link,
-        url:
-          `https://twitter.com/${customId.twitterUserName}/status/${customId.twitterStatusId}`,
+    } else {
+      sendingMessage = {
+        file: {
+          blob: new Blob([await Deno.readFile("spacer.png")], {
+            type: "image/png",
+          }),
+          name: "spacer.png",
+        },
+        components: [row],
       };
-      const row: ActionRow = {
-        type: DiscordMessageComponentTypes.ActionRow,
-        components: [showImagesButton, openTwitterButton],
-      };
-      let sendingMessage: CreateMessage;
-      if (Object.keys(imageUrlMap).length > 1) {
-        sendingMessage = {
-          // Send thumbnail image if more than one tweet url is included
-          // in the original message
-          content: imageUrls[0].replace(/(:[^/]*?)?$/, ":thumb"),
-          components: [row],
-        };
-      } else {
-        sendingMessage = {
-          file: {
-            blob: new Blob([await Deno.readFile("spacer.png")], {
-              type: "image/png",
-            }),
-            name: "spacer.png",
-          },
-          components: [row],
-        };
-      }
-      message.reply(sendingMessage, false);
     }
+    message.reply(sendingMessage, false);
   }
 }
 
@@ -120,16 +170,16 @@ startBot({
 
     async interactionCreate(interaction) {
       if (interaction.type !== DiscordInteractionTypes.MessageComponent) return;
-      const rawCustomId =
-        interaction.message?.components?.[0]?.components?.[0].customId;
+      const rawCustomId = interaction.data?.customId;
       if (!rawCustomId) return;
-      const customId = CustomId.fromString(rawCustomId);
+      const customId = ButtonCustomId.fromString(rawCustomId);
       const message = await helpers.getMessage(
         BigInt(interaction.message?.channelId as string),
         BigInt(customId.discordMessageId as string),
       );
-      const imageUrlMap = getImageUrlMap(message);
-      const imageUrls = imageUrlMap[`${customId.twitterUserName}/${customId.twitterStatusId}`];
+      const imageUrlMap = getImageUrlMap(message, customId.imageSize);
+      const imageUrls =
+        imageUrlMap[`${customId.twitterUserName}/${customId.twitterStatusId}`];
       helpers.sendInteractionResponse(interaction.id, interaction.token, {
         type: DiscordInteractionResponseTypes.ChannelMessageWithSource,
         private: true,
